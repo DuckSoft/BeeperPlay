305d9d78717b269d7c39aa8b16a4db413bf198b3
DuckSoft <ducksoft@foxmail.com>
2017-07-31 04:39:21 +0800

    bpplay.c: adding pause (close #4)
    * new function: use space character for a pause
    * updating ui for new function

--- bpplay.c
+++ bpplay.c
@@ -5,6 +5,8 @@
 
 // kbd
 const char str[] = "zsxdcvgbhnjmq2w3er5t6y7ui9o0p[=]\0";
+// note duration (ms)
+const int note_duration = 180;
 
 // func: beep freq
 void bf(char chr) {
@@ -12,17 +14,25 @@ void bf(char chr) {
 	// freq
 	static const double a = 440.0;	// A440 standard
 	double c = 440.0 * pow(2,3/12);	// 
+	
+	// pause
+	if (chr == ' ') {
+		Sleep(note_duration);
+		return;
+	}
 
 	for (int i=0; str[i]!=0; ++i) 
 		if ((int)str[i] == chr) {
-			Beep(c * pow(2,((double)i)/12.0),180);
+			Beep(c * pow(2,((double)i)/12.0),note_duration);
 			break;
 		}
 }
 
 int main() {
 	// splash
-	printf("input music string.\nkbd: %s\n\n# ",str);
+	printf("input music string.\n"
+	       " kbd: %s\n"
+		   " use Space for a pause.\n# ",str);
 	// alloc
 	static const size_t bufsize = 32767;
 	char *buff = memset(malloc(sizeof(char)*bufsize),0,bufsize);
