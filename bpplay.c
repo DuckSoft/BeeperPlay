de25625db80e0aa8433ebd293e70e9800f450d1a
DuckSoft <ducksoft@foxmail.com>
2017-08-01 00:00:01 +0800

    bpplay.c: adding note sustainer '-'
    * appending one/several '-' to a note now will sustain the note for a duration of one/several notes

--- bpplay.c
+++ bpplay.c
@@ -9,7 +9,7 @@ const char str[] = "zsxdcvgbhnjmq2w3er5t6y7ui9o0p[=]\0";
 const int note_duration = 180;
 
 // func: beep freq
-void bf(char chr) {
+void bf(char chr, int length) {
 	// Twelve-tone equal temperament
 	// freq
 	static const double a = 440.0;	// A440 standard
@@ -23,7 +23,7 @@ void bf(char chr) {
 
 	for (int i=0; str[i]!=0; ++i) 
 		if ((int)str[i] == chr) {
-			Beep(c * pow(2,((double)i)/12.0),note_duration);
+			Beep(c * pow(2,((double)i)/12.0),note_duration * length);
 			break;
 		}
 }
@@ -35,8 +35,13 @@ int main() {
 	static const size_t bufsize = 32767;
 	char *buff = memset(malloc(sizeof(char)*bufsize),0,bufsize);
 	// input and beep
-	while (fgets(buff, bufsize, stdin)) 
-		for (int i=0; buff[i]!=0; bf(buff[i]), ++i);
+	while (fgets(buff, bufsize, stdin)) {
+		int len;	// note length
+		for (int i=0, len = 1; buff[i]!=0; ++i) {
+			for (; buff[i]=='-'; ++i) len++;
+			bf(buff[i],len);
+		}
+	}
 	// dealloc
 	free(buff);
 }
